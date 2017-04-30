de95fb05e003a8fbca4d276d1d21fc5b79e501eb
plusCubed <plusCubed@users.noreply.github.com>
2017-04-30 11:45:06 -0700

    Add working hand-off between scrolling and panning

--- src/pages/solves-bar-component/solves-bar-component.ts
+++ src/pages/solves-bar-component/solves-bar-component.ts
@@ -17,15 +17,15 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
       })),
       state('true', style({
         transform: 'translate3d(0,0px,0)',
-        top: 'calc(-1.2em - 8px)'
+        top: 'calc(0px)'
       })),
       transition('* => true', [
         style({transform: '*', top: '*'}),
-        animate('300ms', style({top: 'calc(-1.2em - 8px)'}))
+        animate('300ms ease-out', style({top: 'calc(0px)'}))
       ]),
       transition('* => false', [
         style({transform: '*', top: '*'}),
-        animate('300ms', style({top: 'calc(100% - 48px - 1.2em - 8px)'}))
+        animate('300ms ease-out', style({top: 'calc(100% - 48px - 1.2em - 8px)'}))
       ]),
     ])
   ]
@@ -35,11 +35,18 @@ export class SolvesBarComponent implements SolvesBar.View {
   private presenter: SolvesBar.Presenter;
   private platform: Platform;
 
-  public offset: number = 0;
-  public lastDy: number = 0;
+  private offset = 0;
+  private lastDy = 0;
 
-  public expandedState: string = "false";
-  public expanded: boolean = false;
+  private scrollTop = 0;
+  private lastScrollTop = 0;
+
+  private expandedState = "false";
+  private expanded = false;
+
+  private scrollEnabled = false;
+  private isScrolling = false;
+  private isAnimating = false;
 
   constructor(solvesService: SolvesService, platform: Platform) {
     this.platform = platform;
@@ -52,26 +59,93 @@ export class SolvesBarComponent implements SolvesBar.View {
       .subscribe(viewModel => this.viewModel = viewModel);
   }
 
+  onArrowClick() {
+    if (this.isAnimating) {
+      return;
+    }
+
+    this.setExpanded(!this.expanded);
+  }
+
+  private setExpanded(expanded: boolean) {
+    this.expanded = expanded;
+    this.expandedState = this.expanded.toString();
+  }
+
+  onSolvesScroll(event) {
+    let dScroll = event.target.scrollTop - this.lastScrollTop;
+
+    if (event.target.scrollTop > 0 || dScroll >= 0) {
+      //If scrolling down or in the middle of scrolling, set flag
+      this.isScrolling = true;
+    } else {
+      //Otherwise switch to moving the sheet
+      this.isScrolling = false;
+      this.scrollEnabled = false;
+    }
+
+    this.offset = 0;
+
+    this.lastScrollTop = event.target.scrollTop;
+  }
+
   @HostListener('panstart', ['$event'])
   onPanStart(event: any) {
+    if (this.isAnimating) {
+      return;
+    }
+
     this.lastDy = 0;
+
+    //If expanded & going upwards, start fake-scrolling
+    if (this.expanded && event.direction == 8) {
+      this.isScrolling = true;
+      this.scrollEnabled = true;
+      this.scrollTop = 0;
+    }
   }
 
   @HostListener('pan', ['$event'])
   onPan(event: any) {
-    this.expandedState = "moving";
-    this.offset += (event.deltaY - this.lastDy);
+    if (this.isAnimating) {
+      return;
+    }
+
+    const dY = event.deltaY - this.lastDy;
+
+    if (!this.isScrolling) {
+      //Moving the sheet
+      this.expandedState = "moving";
+      this.offset = this.offset + dY;
+    } else {
+      //Fake-scrolling
+      this.offset = 0;
+      this.scrollTop -= dY;
+    }
+
     this.lastDy = event.deltaY;
   }
 
   @HostListener('panend', ['$event'])
   onPanEnd(event: any) {
-    this.expanded = (this.offset < -200);
-    this.expandedState = this.expanded.toString();
+    if (this.isAnimating) {
+      return;
+    }
+
+    if (!this.isScrolling) {
+      //If moving the sheet, set expanded status
+      this.setExpanded(this.offset < -200);
+    }
+  }
+
+  @HostListener('@expandedTrigger.start', ['$event'])
+  onAnimationStart(event: any) {
+    this.isAnimating = true;
   }
 
   @HostListener('@expandedTrigger.done', ['$event'])
   onAnimationDone(event: any) {
+    this.isAnimating = false;
     this.offset = 0;
   }
 
@@ -85,7 +159,7 @@ export class SolvesBarComponent implements SolvesBar.View {
     if (!this.expanded) {
       return `calc(100% - 48px - 1.2em - 8px - ${-this.offset}px)`;
     } else {
-      return `calc(-1.2em - 8px - ${-this.offset}px)`;
+      return `calc(${this.offset}px)`;
     }
   }
 
